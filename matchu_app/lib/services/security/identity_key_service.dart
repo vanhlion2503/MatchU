fd039b9473398415e06f69557334bc7dbf65b829
Nguyễn Trần Việt Anh <vietanhnguyen32004@gmail.com>
2026-03-09 16:32:02 +0700

    Update identity_key_service.dart

--- matchu_app/lib/services/security/identity_key_service.dart
+++ matchu_app/lib/services/security/identity_key_service.dart
@@ -1,9 +1,9 @@
 import 'dart:convert';
 import 'dart:math';
-import 'dart:typed_data';
 
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter/foundation.dart';
 import 'package:flutter_secure_storage/flutter_secure_storage.dart';
 import 'package:matchu_app/services/security/device_service.dart';
 import 'package:pointycastle/asn1/primitives/asn1_integer.dart';
@@ -14,54 +14,108 @@ class IdentityKeyService {
   static final _storage = FlutterSecureStorage();
   static final _db = FirebaseFirestore.instance;
 
-  static String _privateKeyKey(String uid, String deviceId) => 'identity_${uid}_$deviceId';
+  static String _privateKeyKey(String uid, String deviceId) =>
+      'identity_${uid}_$deviceId';
 
   /// ===============================
   /// CHECK EXIST
   /// ===============================
   static Future<bool> hasIdentityKey() async {
-    final uid = FirebaseAuth.instance.currentUser!.uid;
+    final user = FirebaseAuth.instance.currentUser;
+    if (user == null) return false;
+
+    final uid = user.uid;
     final deviceId = await DeviceService.getDeviceId();
 
-    final key = await _storage.read(
-      key: _privateKeyKey(uid, deviceId),
-    );
+    final key = await _storage.read(key: _privateKeyKey(uid, deviceId));
     return key != null;
   }
-
 
   /// ===============================
   /// GENERATE RSA 2048
   /// ===============================
   static Future<void> generateIfNotExists() async {
-    final uid = FirebaseAuth.instance.currentUser!.uid;
+    final user = FirebaseAuth.instance.currentUser;
+    if (user == null) return;
+
+    final uid = user.uid;
     final deviceId = await DeviceService.getDeviceId();
     final keyKey = _privateKeyKey(uid, deviceId);
 
-    final exists = await _storage.read(key: keyKey);
-    if (exists != null) return;
+    String? privatePem = await _storage.read(key: keyKey);
+    late final String publicPem;
 
-    final pair = _generateRSAKeyPair();
+    if (privatePem == null) {
+      final pair = _generateRSAKeyPair();
+      privatePem = _encodePrivateKeyToPem(pair.privateKey as RSAPrivateKey);
+      publicPem = _encodePublicKeyToPem(pair.publicKey as RSAPublicKey);
 
-    await _storage.write(
-      key: keyKey,
-      value: _encodePrivateKeyToPem(pair.privateKey as RSAPrivateKey),
-    );
+      await _storage.write(key: keyKey, value: privatePem);
+    } else {
+      publicPem = _extractPublicKeyFromPrivatePem(privatePem);
+    }
 
-    await _db
+    await _ensureDeviceDoc(uid: uid, deviceId: deviceId, publicPem: publicPem);
+  }
+
+  static Future<void> _ensureDeviceDoc({
+    required String uid,
+    required String deviceId,
+    required String publicPem,
+  }) async {
+    final docRef = _db
         .collection('users')
         .doc(uid)
         .collection('devices')
-        .doc(deviceId)
-        .set({
-      'publicKey': _encodePublicKeyToPem(pair.publicKey as RSAPublicKey),
+        .doc(deviceId);
+    final snap = await docRef.get();
+
+    if (snap.exists) {
+      await docRef.update({'lastActiveAt': FieldValue.serverTimestamp()});
+      return;
+    }
+
+    await docRef.set({
+      'publicKey': publicPem,
       'algorithm': 'RSA-2048',
+      'platform': _platformName(),
       'createdAt': FieldValue.serverTimestamp(),
       'lastActiveAt': FieldValue.serverTimestamp(),
     });
   }
 
+  static String _platformName() {
+    if (kIsWeb) return 'web';
 
+    switch (defaultTargetPlatform) {
+      case TargetPlatform.android:
+        return 'android';
+      case TargetPlatform.iOS:
+        return 'ios';
+      case TargetPlatform.macOS:
+        return 'macos';
+      case TargetPlatform.windows:
+        return 'windows';
+      case TargetPlatform.linux:
+        return 'linux';
+      case TargetPlatform.fuchsia:
+        return 'fuchsia';
+    }
+  }
+
+  static String _extractPublicKeyFromPrivatePem(String pem) {
+    final clean = pem
+        .replaceAll('-----BEGIN RSA PRIVATE KEY-----', '')
+        .replaceAll('-----END RSA PRIVATE KEY-----', '')
+        .replaceAll(RegExp(r'\s'), '');
+
+    final bytes = base64Decode(clean);
+    final seq = ASN1Sequence.fromBytes(bytes);
+
+    final modulus = (seq.elements![1] as ASN1Integer).integer!;
+    final exponent = (seq.elements![2] as ASN1Integer).integer!;
+    return _encodePublicKeyToPem(RSAPublicKey(modulus, exponent));
+  }
 
   /// ===============================
   /// RSA KEY GENERATION
@@ -77,17 +131,13 @@ class IdentityKeyService {
 
     secureRandom.seed(KeyParameter(seed));
 
-    final keyGen = RSAKeyGenerator()
-      ..init(
-        ParametersWithRandom(
-          RSAKeyGeneratorParameters(
-            BigInt.parse('65537'),
-            2048,
-            64,
+    final keyGen =
+        RSAKeyGenerator()..init(
+          ParametersWithRandom(
+            RSAKeyGeneratorParameters(BigInt.parse('65537'), 2048, 64),
+            secureRandom,
           ),
-          secureRandom,
-        ),
-      );
+        );
 
     return keyGen.generateKeyPair();
   }
@@ -104,16 +154,17 @@ class IdentityKeyService {
     final BigInt dq = d % (q - BigInt.one);
     final BigInt qi = q.modInverse(p);
 
-    final topLevel = ASN1Sequence()
-      ..add(ASN1Integer(BigInt.zero))
-      ..add(ASN1Integer(key.n))
-      ..add(ASN1Integer(key.publicExponent))
-      ..add(ASN1Integer(d))
-      ..add(ASN1Integer(p))
-      ..add(ASN1Integer(q))
-      ..add(ASN1Integer(dp))
-      ..add(ASN1Integer(dq))
-      ..add(ASN1Integer(qi));
+    final topLevel =
+        ASN1Sequence()
+          ..add(ASN1Integer(BigInt.zero))
+          ..add(ASN1Integer(key.n))
+          ..add(ASN1Integer(key.publicExponent))
+          ..add(ASN1Integer(d))
+          ..add(ASN1Integer(p))
+          ..add(ASN1Integer(q))
+          ..add(ASN1Integer(dp))
+          ..add(ASN1Integer(dq))
+          ..add(ASN1Integer(qi));
 
     final dataBase64 = base64Encode(topLevel.encode());
     return '''
@@ -124,23 +175,22 @@ class IdentityKeyService {
   }
 
   static String _encodePublicKeyToPem(RSAPublicKey key) {
-    final seq = ASN1Sequence()
-      ..add(ASN1Integer(key.n))
-      ..add(ASN1Integer(key.exponent));
+    final seq =
+        ASN1Sequence()
+          ..add(ASN1Integer(key.n))
+          ..add(ASN1Integer(key.exponent));
 
     final dataBase64 = base64Encode(seq.encode());
     return '-----BEGIN RSA PUBLIC KEY-----\n$dataBase64\n-----END RSA PUBLIC KEY-----';
   }
 
   static Future<String?> readPrivateKey() async {
-    final uid = FirebaseAuth.instance.currentUser!.uid;
+    final user = FirebaseAuth.instance.currentUser;
+    if (user == null) return null;
+
+    final uid = user.uid;
     final deviceId = await DeviceService.getDeviceId();
 
-    return _storage.read(
-      key: _privateKeyKey(uid, deviceId),
-    );
+    return _storage.read(key: _privateKeyKey(uid, deviceId));
   }
-
-
-
 }
